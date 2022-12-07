2949aea6baef506839ef0c191b19b6ba7db594c9
leeyunhyuk <leeyunhyuk104@gmail.com>
2022-12-08 01:49:21 +0900

    section7 test add

--- golang_example/go-section7/test.go
+++ golang_example/go-section7/test.go
@@ -14,6 +14,16 @@ func makesum(a int, b int) (int, string) {
 	return 100, "test"
 }
 
+func callback(test int, f func(*int)) {
+	fmt.Println("call back & number : ", test)
+	f(&test)
+	fmt.Println("after call back & number : ", test)
+}
+
+func mul_reference(num *int) {
+	*num = *num * 10
+}
+
 func main() {
 
 	a, b := makesum(1, 1)
@@ -21,4 +31,9 @@ func main() {
 
 	fmt.Println(a, b)
 	fmt.Println(c)
+
+	val := 5
+	var ptr *int = &val
+	callback(*ptr, mul_reference)
+
 }
